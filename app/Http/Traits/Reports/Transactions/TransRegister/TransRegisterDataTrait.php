751a766c57674ca2bd7011191742fa57c113a811
unknown <christopher.boyles@asciisolution.tech>
2025-01-16 01:28:36 +0800

    TransRegisterDataTrait minimal Changes

--- app/Http/Traits/Reports/Transactions/TransRegister/TransRegisterDataTrait.php
+++ app/Http/Traits/Reports/Transactions/TransRegister/TransRegisterDataTrait.php
@@ -47,7 +47,7 @@ Trait TransRegisterDataTrait
         $item['mem_code'] = $toMembership->mem_code;
         $item['loan_type'] = $toLoan->loan_code . ' - '. $toLoan->loan_name;
         $item['collat_name'] = $toCollateral->collat_name;
-        $item['status'] = $toApproval->status == 'APRV' ? 'Approved' : 'Rejected';
+        $item['status'] = ($toApproval?->status ?? '') === 'APRV' ? 'Approved' : 'Rejected';
         $item['principal_amt'] = number_format($item->principal_amt, 2);
         $item['total_interest'] = number_format($item->total_interest, 2);
         $item['service_deduction'] = number_format($item->service_deduction, 2);
@@ -55,7 +55,7 @@ Trait TransRegisterDataTrait
         $item['net_amt'] = number_format($item->net_amt, 2);
         $item['encoded_by'] = $toUser->first_name . ' '  . $toUser->last_name;
         $item['date_created'] = date('m/d/Y', strtotime($item->created_at));
-        $item['date_maturity'] = $toApproval->end_date != NULL ? date('m/d/Y', strtotime($toApproval->end_date ?? null)) : 'N/A';
+        $item['date_maturity'] = ($toApproval?->end_date ?? '') != NULL ? date('m/d/Y', strtotime($toApproval->end_date ?? null)) : 'N/A';
 
         return $item;
     }
